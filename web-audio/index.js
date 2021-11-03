012d5ad7d7d9b3fd0b0f6a999332f37f788cae08
tortorse <tortorse@gmail.com>
2021-11-03 13:34:28 +0800

    feat: 添加音量控制

--- web-audio/index.js
+++ web-audio/index.js
@@ -1,21 +1,35 @@
+/*
+ * @Date: 2021-11-02 16:49:52
+ * @LastEditors: tortorse
+ * @LastEditTime: 2021-11-03 13:02:55
+ * @FilePath: \web-samples\web-audio\index.js
+ */
 const music = new Audio("music.mp3");
 
 const playButton = document.querySelector("#play");
+const pauseButton = document.querySelector("#pause");
 const stopButton = document.querySelector("#stop");
+const volumeHandler = document.querySelector("#volume");
 
 playButton.addEventListener("click", () => {
   play();
+});
+
+pauseButton.addEventListener("click", () => {
+  pause();
 });
 
 stopButton.addEventListener("click", () => {
   stop();
 });
 
-
+volumeHandler.addEventListener("input", (e) => {
+  music.volume = e.target.value / 100;
+});
 
 music.addEventListener("canplay", () => {
   document.querySelector("#progress").max = music.duration;
-})
+});
 
 music.addEventListener("timeupdate", () => {
   document.querySelector("#progress").value = music.currentTime;
@@ -28,4 +42,8 @@ const play = () => {
 const stop = () => {
   music.pause();
   music.currentTime = 0;
+};
+
+const pause = () => {
+  music.pause();
 };
