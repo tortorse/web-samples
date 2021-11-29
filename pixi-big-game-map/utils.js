a33e9a003f47af6fd2147a8e3ccb8e25165ffa79
tortorse <tortorse@gmail.com>
2021-11-29 18:43:34 +0800

    chore: make data

--- pixi-big-game-map/utils.js
+++ pixi-big-game-map/utils.js
@@ -1,22 +1,44 @@
-const originX = 48
-let id = 326
-let x = 48
-let y
-let areaId = 326
-let size = 12
-let row = []
-for (let j = 0; j < size; j++) {
-  for (let i = 0; i < size; i++) {
-    if (x === size - 1) {
-      x = originX
+function mockArea() {
+  const originX = 156;
+  let x = 156;
+  const areaId = 1189;
+  let id = 1189;
+  let size = 12;
+  let row = [];
+  for (let j = 0; j < size; j++) {
+    for (let i = 0; i < size; i++) {
+      if (x === size - 1) {
+        x = originX;
+      }
+      id += 1;
+      row.push({
+        id,
+        position: { x: originX + i, y: j },
+        locatedIn: areaId,
+      });
     }
-    id += 1;
-    row.push({
-      id,
-      position: { x: originX + i, y: j },
-      locatedIn: areaId
-    });
   }
 }
 
-
+function mockCells() {
+  let row = [];
+  let id = 1483;
+  let count = 12;
+  let startX = 143;
+  let color = "2ac161";
+  let y = 1;
+  for (let i = 0; i < count; i++) {
+    id += 1;
+    row.push({
+      id,
+      size: 1,
+      position: {
+        x: startX + 1 + i,
+        y,
+      },
+      content: {
+        color,
+      },
+    });
+  }
+}
