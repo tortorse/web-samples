d976c8d7f0bb7ae72a6ca03765bf05beebcb0d25
tortorse <tortorse@gmail.com>
2022-01-04 16:55:21 +0800

    feat: find rect

--- draw-gemetries/index.js
+++ draw-gemetries/index.js
@@ -1,15 +1,19 @@
 const data = {
+  id: 4,
   tag: "GuiLayout",
   children: [
     {
+      id: 203,
       tag: "FormItem",
       coordinates: { minX: 48, minY: 48, maxX: 467.515625, maxY: 87.984375 },
     },
     {
+      id: 394,
       tag: "Input",
       coordinates: { minX: 128, minY: 52, maxX: 467.515625, maxY: 84 },
     },
     {
+      id: 210,
       tag: "FormItem",
       coordinates: {
         minX: 515.515625,
@@ -19,10 +23,12 @@ const data = {
       },
     },
     {
+      id: 419,
       tag: "Input",
       coordinates: { minX: 595.515625, minY: 48, maxX: 924.46875, maxY: 80 },
     },
     {
+      id: 409,
       tag: "FormItem",
       coordinates: {
         minX: 972.46875,
@@ -32,10 +38,12 @@ const data = {
       },
     },
     {
+      id: 411,
       tag: "Input",
       coordinates: { minX: 1052.46875, minY: 52, maxX: 1391.984375, maxY: 84 },
     },
     {
+      id: 232,
       tag: "FormItem",
       coordinates: {
         minX: 48,
@@ -45,6 +53,7 @@ const data = {
       },
     },
     {
+      id: 234,
       tag: "Select",
       coordinates: {
         minX: 98,
@@ -54,10 +63,7 @@ const data = {
       },
     },
     {
-      tag: "Option",
-      coordinates: { minX: -504.5, minY: -200, maxX: -504.5, maxY: -200 },
-    },
-    {
+      id: 416,
       tag: "Button",
       coordinates: {
         minX: 515.515625,
@@ -67,6 +73,7 @@ const data = {
       },
     },
     {
+      id: 417,
       tag: "Button",
       coordinates: {
         minX: 583.515625,
@@ -76,12 +83,14 @@ const data = {
       },
     },
     {
+      id: 193,
       tag: "TableTemplateView",
-      coordinates: { minX: 48, minY: 224.96875, maxX: 1392, maxY: 342.96875 },
+      coordinates: { minX: 48, minY: 224.96875, maxX: 1392, maxY: 340.96875 },
     },
     {
+      id: 195,
       tag: "Page",
-      coordinates: { minX: 1120, minY: 366.96875, maxX: 1392, maxY: 398.96875 },
+      coordinates: { minX: 1120, minY: 364.96875, maxX: 1392, maxY: 396.96875 },
     },
   ],
   coordinates: { minX: 504.5, minY: 200, maxX: 1944.5, maxY: 1069 },
@@ -91,7 +100,7 @@ const rootDom = document.createElement("div");
 rootDom.style.width = `${data.coordinates.maxX - data.coordinates.minX}px`;
 rootDom.style.height = `${data.coordinates.maxY - data.coordinates.minY}px`;
 rootDom.style.position = "relative";
-rootDom.style.backgroundColor = randomColor();
+// rootDom.style.backgroundColor = randomColor();
 document.body.appendChild(rootDom);
 
 data.children.forEach((rect) => {
@@ -106,8 +115,58 @@ data.children.forEach((rect) => {
   rootDom.appendChild(rectDom);
 });
 
-function randomColor(isOpaque) {
-  const alpha = isOpaque ? "50" : "FF";
-  const color = Math.floor(Math.random() * 16777215).toString(16);
-  return `#${color}${alpha}`;
+function randomColor(opacity) {
+  let color = Math.floor(Math.random() * 16777215).toString(16);
+  let alpha = opacity ? "80" : "FF";
+  const hexColor = "#" + ("000000" + color).slice(-6) + alpha;
+  return hexColor;
+}
+
+const rTree = new RBush(16);
+console.log(rTree);
+data.children.forEach((item) => {
+  rTree.insert({ ...item.coordinates, tag: item.tag, id: item.id });
+});
+
+let rectangles = [];
+let lastId;
+rTree.data.children.forEach((rect) => {
+  const neighbors = knn(
+    rTree,
+    rect.maxX,
+    rect.maxY,
+    2,
+    (item) => item.id !== rect.id
+  );
+  const neighbor = neighbors[0];
+  console.log(rect.tag, rect.id, neighbor);
+  const rectangleId = rect.id + "|" + neighbor.id;
+  const rectangle = {
+    id: rectangleId,
+    minX: Math.min(rect.minX, neighbor.minX),
+    maxX: Math.max(rect.maxX, neighbor.maxX),
+    minY: Math.min(rect.minY, neighbor.minY),
+    maxY: Math.max(rect.maxY, neighbor.maxY),
+  };
+  if (getReverseId(rectangleId) !== lastId) {
+    rectangles.push(rectangle);
+    lastId = rectangleId;
+  }
+});
+
+rectangles.forEach((rect) => {
+  const rectDom = document.createElement("div");
+  rectDom.style.position = "absolute";
+  rectDom.style.width = `${rect.maxX - rect.minX}px`;
+  rectDom.style.height = `${rect.maxY - rect.minY}px`;
+  rectDom.style.left = `${rect.minX}px`;
+  rectDom.style.top = `${rect.minY}px`;
+  rectDom.style.borderColor = "#ff00ff";
+  rectDom.style.borderWidth = "1px";
+  rectDom.style.borderStyle = "solid";
+  rootDom.appendChild(rectDom);
+});
+
+function getReverseId(id) {
+  return id.split("|").reverse().join("|");
 }
