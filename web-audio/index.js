50a4dea498c1c8254a4d5ccecfe02418178d45dd
tortorse <tortorse@gmail.com>
2022-03-01 20:53:26 +0800

    feat: 添加 audioContext

--- web-audio/index.js
+++ web-audio/index.js
@@ -47,3 +47,43 @@ const stop = () => {
 const pause = () => {
   music.pause();
 };
+
+// audiocontext
+const AudioContext = window.AudioContext || window.webkitAudioContext;
+
+const audioContext = new AudioContext();
+
+const playAudioButton = document.querySelector("#play-audio");
+const stopAudioButton = document.querySelector("#stop-audio");
+playAudioButton.addEventListener("click", async () => {
+  await playAudio();
+});
+
+stopAudioButton.addEventListener("click", () => {
+  stopAudio();
+});
+
+let source;
+
+async function playAudio() {
+  source = audioContext.createBufferSource();
+  await getAudioData();
+  source.start(0);
+}
+
+function stopAudio() {
+  source.stop(0);
+  source.disconnect(audioContext.destination);
+  source = null;
+}
+
+async function getAudioData() {
+  const res = await fetch("music.mp3", { method: "GET" });
+  const arrayBuffer = await res.arrayBuffer();
+  audioContext.decodeAudioData(arrayBuffer, (decoded) => {
+    if (!source.buffer) {
+      source.buffer = decoded;
+    }
+    source.connect(audioContext.destination);
+  });
+}
